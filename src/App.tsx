6f69faf12a29619d2add5d13e10ee67b7534c544
Trevor <valentinosolischang@gmail.com>
2025-04-18 17:38:43 -0500

    feat: wrap main layout with SecretProvider for improved context management

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import AuthForm from "./components/Auth/AuthForm";
 import LogOut from "./components/Auth/LogOut";
 import SecretForm from "./components/Vault/SecretForm";
+import Vault from "./components/Vault/Vault";
 import AuthProvider from "./contexts/AuthContext/AuthProvider";
+import SecretProvider from "./contexts/SecretContext/SecretProvider";
 
 function App() {
   return (
     <AuthProvider>
-      <main className="flex flex-col gap-10 w-full">
-        <div className="flex flex-col gap-10">
-          <AuthForm />
-          <LogOut />
-        </div>
-        <SecretForm />
-      </main>
+      <SecretProvider>
+        <main className="flex flex-col gap-10 w-full">
+          <div className="flex flex-col gap-10">
+            <AuthForm />
+            <LogOut />
+          </div>
+          <div className="flex flex-col gap-10">
+            <SecretForm />
+            <Vault />
+          </div>
+        </main>
+      </SecretProvider>
     </AuthProvider>
   );
 }
