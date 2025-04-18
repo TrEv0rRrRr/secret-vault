431dbc2b8eac700972e5eee36b800430280adefb
Trevor <valentinosolischang@gmail.com>
2025-04-18 17:38:21 -0500

    feat: remove localStorageHelpers for secrets management

--- src/helpers/localStorageHelpers.ts
+++
@@ -1,5 +0,0 @@
-export const saveSecrets = (secrets: string[]) =>
-  localStorage.setItem("secrets", JSON.stringify(secrets));
-
-export const getSecrets = (): string[] =>
-  JSON.parse(localStorage.getItem("secrets") || "[]");
