2268acda7f19afc51ba9fc1a1f0a3db205772be1
Trevor <valentinosolischang@gmail.com>
2025-04-18 17:57:56 -0500

    feat: add LoginReminder component to guide users on account creation for saving secrets

---
+++ src/components/Vault/LoginReminder.tsx
@@ -0,0 +1,10 @@
+const LoginReminder = () => {
+  return (
+    <p className="text-white">
+      To save new secrets you have to create an account. <br />
+      Already have one? Just log in.
+    </p>
+  );
+};
+
+export default LoginReminder;
