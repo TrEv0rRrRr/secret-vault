74446f3443ad6af8e2cb3fd65b60236b84aea28c
Trevor <valentinosolischang@gmail.com>
2025-04-18 16:44:02 -0500

    feat: restructure App component to include AuthProvider and main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import AuthForm from "./components/Auth/AuthForm";
+import LogOut from "./components/Auth/LogOut";
+import SecretForm from "./components/Vault/SecretForm";
+import AuthProvider from "./contexts/AuthContext/AuthProvider";
+
 function App() {
-  return <div className="bg-amber-200">xd</div>;
+  return (
+    <AuthProvider>
+      <main className="flex flex-col gap-10 w-full">
+        <div className="flex flex-col gap-10">
+          <AuthForm />
+          <LogOut />
+        </div>
+        <SecretForm />
+      </main>
+    </AuthProvider>
+  );
 }
 
 export default App;
